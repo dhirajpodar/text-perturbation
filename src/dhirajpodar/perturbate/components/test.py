f5f368dfcf9c27a5575f26e82c88e484d3fa418a
dhirajpoddar <dhirajpodar@gmail.com>
2024-08-12 21:12:32 +0200

    project structure changed

--- src/dhirajpodar/perturbate/components/test.py
+++
@@ -1,8 +0,0 @@
-from perturbation import Perturbate
-
-text = "hello, how are you?"
-
-perturbate = Perturbate(text)
-perturbated = perturbate.call_by_type('random_char_insertion')
-print(f"Original:: {text}")
-print(f"perturbated:: {perturbated}")
